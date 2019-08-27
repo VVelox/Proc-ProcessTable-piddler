73d251bb502f76a9eaf2411aa45fc5a7072768f5
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2019-08-27 01:03:24 -0500

    create base module stuff

---
+++ Proc-ProcessTable-piddler/lib/Proc/ProcessTable/piddler.pm
@@ -0,0 +1,109 @@
+package Proc::ProcessTable::piddler;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Proc::ProcessTable::piddler - The great new Proc::ProcessTable::piddler!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Proc::ProcessTable::piddler;
+
+    my $foo = Proc::ProcessTable::piddler->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-proc-processtable-piddler at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Proc-ProcessTable-piddler>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Proc::ProcessTable::piddler
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Proc-ProcessTable-piddler>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Proc-ProcessTable-piddler>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/Proc-ProcessTable-piddler>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/Proc-ProcessTable-piddler>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2019 by Zane C. Bowers-Hadley.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+
+=cut
+
+1; # End of Proc::ProcessTable::piddler
